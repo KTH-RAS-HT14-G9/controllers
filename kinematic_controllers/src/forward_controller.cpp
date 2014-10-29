f5c12511fb08c1a28182c0ea6659264f91d3d0fc
Max Losch <mmlosch@kth.se>
2014-10-29 09:53:38 +0100

    add p controlling

--- kinematic_controllers/src/forward_controller.cpp
+++ kinematic_controllers/src/forward_controller.cpp
@@ -2,12 +2,15 @@
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Float64.h>
 #include <std_msgs/Bool.h>
+#include <../etc/pid.h>
 
 const double PUBLISH_FREQUENCY = 10.0;
 const double DEFAULT_VELOCITY = 0.5;
 
 double _velocity = DEFAULT_VELOCITY;
 bool _active;
+
+double _kp = 1.0; std::string _kp_key="/controller/forward/kp";
 
 void callback_forward_velocity(const std_msgs::Float64ConstPtr& vel) {
     _velocity = vel->data;
@@ -26,16 +29,20 @@ int main(int argc, char **argv)
     ros::Subscriber sub_act = n.subscribe("/controller/forward/active",   1, callback_activate);
     ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/controller/forward/twist", 1);
 
+    n.setParam(_kp_key, _kp);
+
     ros::Rate loop_rate(PUBLISH_FREQUENCY);
+
+    geometry_msgs::Twist twist;
 
     while (ros::ok()) {
 
-        geometry_msgs::Twist twist;
+        n.getParamCached(_kp_key, _kp);
 
         if (_active)
-            twist.linear.x = _velocity;
+            twist.linear.x += pd::P_control(_kp, twist.linear.x, _velocity);
         else
-            twist.linear.x = 0;
+            twist.linear.x += pd::P_control(_kp, twist.linear.x, 0);
 
         pub.publish(twist);
 
