b72c39def80f054ca4ec53dd361b2735921572f9
Max Losch <mmlosch@kth.se>
2014-10-29 15:07:21 +0100

    making constants const

--- etc/robot.h
+++ etc/robot.h
@@ -6,16 +6,16 @@
 namespace robot {
 
 namespace dim {
-    double robot_diameter = 0.23;
-    double robot_height = 0.28;
-    double wheel_radius = 0.049;
-    double wheel_distance = 0.21;
+    const double robot_diameter = 0.23;
+    const double robot_height = 0.28;
+    const double wheel_radius = 0.049;
+    const double wheel_distance = 0.21;
 }
 
 namespace prop {
-    double ticks_per_rev = 360.0;
-    double max_velocity = 5.0;
-    double max_rot_velocity = M_PI/4.0;
+    const double ticks_per_rev = 360.0;
+    const double max_velocity = 5.0;
+    const double max_rot_velocity = M_PI/4.0;
 }
 
 }
