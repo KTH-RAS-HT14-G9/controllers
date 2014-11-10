abf16136c1cf23948e40d725d3683bd2cbfb1a18
Tobias Andersson <tobias.ason@gmail.com>
2014-11-10 11:56:46 +0100

    Wall follower almost working. Issue when the robot is too close to the right wall

--- kinematic_controllers/src/wall_following_controller.cpp
+++ kinematic_controllers/src/wall_following_controller.cpp
@@ -8,12 +8,14 @@
 #include <pid.h>
 #include <common/robot.h>
 #include <common/parameter.h>
+#include <std_msgs/Bool.h>
 
 int _ch1,_ch2,_ch3,_ch4;
 double _d1,_d2,_d3,_d4;
+bool _active;
 
-Parameter<double> _kp("/controller/wall_follow/kp", -3);
-Parameter<double> _kp2("/controller/wall_follow/kp2", -10);
+Parameter<double> _kp("/controller/wall_follow/kp", 3);
+Parameter<double> _kp2("/controller/wall_follow/kp2", 10);
 
 /**
  * Function that will be executed when the values from the distance
@@ -35,6 +37,10 @@ void sensorCallback(const ras_arduino_msgs::ADConverter lecture)
   ROS_INFO("Distance br(4): %f\n", _d4);
 }
 
+void callback_activate(const std_msgs::BoolConstPtr& val) {
+    _active = val->data;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -53,6 +59,7 @@ int main(int argc, char **argv)
    * Subscribe to the topic "/arduino/adc"
    */
   ros::Subscriber sub = n.subscribe("/arduino/adc",1000,sensorCallback);
+  ros::Subscriber sub_act = n.subscribe("/controller/wall_follower/active", 1, callback_activate);
 
   ros::Rate loop_rate(10);
 
@@ -80,25 +87,31 @@ int main(int argc, char **argv)
 
   while (ros::ok())
   {
-    if (_d1 < 0.1) //front left side < 0.1
-    {
-      //twist.angular.z = _kp*(_d1-_d3);
-      state = _d3;
-      target = _d1;
-      twist.angular.z = pd::P_control(_kp2(),state,target);
-    } else if (_ch2 < 0.1) //front right side < 0.1
-    {
-      //twist.angular.z = -_kp*(_d2-_d4);
-      state = _d4;
-      target = _d2;
-      twist.angular.z = -pd::P_control(_kp2(),state,target);
-    } else
-    {
-      //twist.angular.z = _kp*((_d1+_d4)-(_d2+_d3));
-      state = _d2+_d3;
-      target = _d1+_d4;
-      twist.angular.z = pd::P_control(_kp(),state,target);
-    }
+	if (_active)
+	{
+		if (_d1 < 0.1) //front left side < 0.1
+		{
+		  //twist.angular.z = _kp*(_d1-_d3);
+		  state = _d3;
+		  target = _d1;
+		  twist.angular.z = pd::P_control(_kp2(),state,target);
+		} else if (_ch2 < 0.1) //front right side < 0.1
+		{
+		  //twist.angular.z = -_kp*(_d2-_d4);
+		  state = _d4;
+		  target = _d2;
+		  twist.angular.z = -pd::P_control(_kp2(),state,target);
+		} else
+		{
+		  //twist.angular.z = _kp*((_d1+_d4)-(_d2+_d3));
+		  state = _d2+_d3;
+		  target = _d1+_d4;
+		  twist.angular.z = pd::P_control(_kp(),state,target);
+		}
+	} else
+	{
+		twist.angular.z = 0;
+	}
     
 
     pub.publish(twist);
